edcc9d7a4a9726d7081c8c7543ba646dc51c8045
Justin Paulson <justinapaulson@gmail.com>
2024-11-28 00:14:32 -0600

    push start game button when players arrive

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,6 @@
 class Player < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
   belongs_to :user
   belongs_to :game
   has_many :scores, through: :user
@@ -16,5 +18,13 @@ class Player < ApplicationRecord
       target: "players",
       partial: "games/players",
       locals: { game: game }
+
+    if game.players.count > 2
+      broadcast_replace_to [ game, "players" ],
+        target: "start",
+        html: "<div class='mt-4'>
+  <a href=#{start_game_path(game)} class='rounded bg-blue-600 text-white p-2 hover:bg-blue-700'>Start Game</a>
+</div>"
+    end
   end
 end
