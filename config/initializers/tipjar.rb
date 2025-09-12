856eb5468957ce34ed8e4dd424976a9c9b6d56ad
Justin Paulson <justinapaulson@gmail.com>
2025-09-12 09:45:59 -0500

    update tip jar to 1.0.1

--- config/initializers/tipjar.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-RailsTipjar.configure do |config|
-  config.payment_link = "https://buy.stripe.com/9B600i4VY3Be3CT4AOfrW00"
-  config.icon = :jar
-  config.analytics_enabled = false
-end
